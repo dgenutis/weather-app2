4556af9de88de4f55d0930ba28ce56aae6615b42
dgenutis <dainiusgenutis@yahoo.com>
2024-10-14 11:55:33 +0300

    API fetch configuration

--- assets/js/api.js
+++ assets/js/api.js
@@ -4,3 +4,41 @@
  * @copyright Dainius Genutis 2024 All rights reserved
  * @author Dainius Genutis <dainiusgenutis@yahoo.com>
  */
+
+
+'use strict';
+
+const api_url = "bc6340660c601debbabce712acabba9b";
+
+/**
+ * @param {string} URL API url
+ * @param {Function} callback callback
+ */
+export const fetchData = function (URL, callback) {
+    fetch(`${URL}&appid=${api_key}`)
+       .then(res => res.json())
+       .then(data => callback(data));
+}
+
+export const url = {
+    currentWeather(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`;
+    },
+    forecast(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`;
+    },
+    airPollution(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
+    },
+    reverseGeo(lat, lon) {
+        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`;
+    },
+    /**
+     * 
+     * @param {string} query Search query e.g.: "London", "New York" 
+     */
+
+    geo(query) {
+        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
+    }
+}
